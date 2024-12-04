118704bb85d76c54a50d0ffcebeccb8c2b1b4bbf
diyajojo <diyaj2122@gmail.com>
2024-12-04 23:27:10 +0530

    done

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -33,7 +33,7 @@ export default async function Page({
 
     return (
       <div className="min-h-screen bg-black">
-        <RoastPage profiledata={profiledata} personality={personality} />
+        <RoastPage profiledata={profiledata} personality={personality || 'savage'} />
       </div>
     );
   } catch (error) {
